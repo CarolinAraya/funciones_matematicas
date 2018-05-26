10860833167ba19c74b293dab43eea4b1d62c884
Carolina Araya A <_carolina_@hotmail.cl>
2018-05-26 10:12:30 -0400

    formulacion de las funciones

---
+++ app.js
@@ -0,0 +1,16 @@
+// formulacion de las funciones
+
+//36325*(9824+777);
+let resultado = "";
+
+function add(a, b) {
+    let suma = 9824 + 777;
+}
+
+function multiply(x, y) {
+    let producto = 36325 * suma;
+    let resultado = producto.toString();
+}
+
+
+
