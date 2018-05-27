c8c84ba27aca52d920df084467d6ee1fbc4a5f88
Carolina Araya A <_carolina_@hotmail.cl>
2018-05-27 11:00:49 -0400

    Arreglé la función, da resultado. Importante: return

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // formulacion de las funciones
 
 //36325*(9824+777);
-let resultado = "";
+
 
 function add(a, b) {
-    let suma = 9824 + 777;
+    return a + b;
 }
 
 function multiply(x, y) {
-    let producto = 36325 * suma;
-    let resultado = producto.toString();
+    return  x* y;
 }
+
+
+let resultado = multiply(36325, add(9824,777));
+
+
+
 
 
 
